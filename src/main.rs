226a1bbcf21b20c78e1e5a7f3d6e658741150a0f
Anthony Nguyen <anknguyen@gmail.com>
2016-06-20 10:11:00 -0400

    Improve colour output and directory traversal logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate ansi_term;
 
 use ansi_term::Colour;
+use ansi_term::Style;
 
 use std::env;
 use std::fs;
@@ -32,40 +33,40 @@ impl IsDir for path::PathBuf {
 }
 
 trait DisplayColour {
-    fn display_colour(&self) -> String;
+    fn display_colour(&self, dstyle: Style, fstyle: Style, short: bool) -> String;
 }
 
 impl DisplayColour for path::PathBuf {
-    fn display_colour(&self) -> String {
-        match self.is_dir() {
-            true => Colour::Yellow.bold().paint(self.to_str().unwrap()).to_string(),
-            false => String::from(self.to_str().unwrap()),
+    fn display_colour(&self, dstyle: Style, fstyle: Style, short: bool) -> String {
+        let style: Style;
+        if self.is_dir() {
+            style = dstyle;
+        } else {
+            style = fstyle;
+        }
+
+        if short {
+            style.paint(self.file_name().unwrap().to_str().unwrap()).to_string()
+        } else {
+            style.paint(self.to_str().unwrap()).to_string()
         }
     }
 }
 
 fn explore(path: &path::PathBuf) -> () {
-    if !path.is_dir() {
-        println!("{}", path.display_colour());
+    if !path.is_dir() ||
+       IGNORE_PATHS.contains(&path.file_name().unwrap().to_str().unwrap()) {
         return;
     }
-
-    if IGNORE_PATHS.contains(&path.file_name().unwrap().to_str().unwrap()) {
-        return;
-    }
-    println!("{}:", path.display_colour());
+    println!("{}:", path.display_colour(Colour::Yellow.bold(), Style::default(), false));
 
     let mut q: Vec<path::PathBuf> = Vec::new();
     for item in fs::read_dir(path).unwrap() {
         let f = item.unwrap(); // f is a DirEntry
-        match f.is_dir() {
-            true => {
-                q.push(f.path());
-            },
-            false => {
-                explore(&f.path());
-            },
+        if f.is_dir() {
+            q.push(f.path());
         };
+        println!("{}", f.path().display_colour(Colour::Cyan.normal(), Style::default(), true));
     }
 
     print!("\n");
